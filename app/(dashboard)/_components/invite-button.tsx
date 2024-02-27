8f15bf33aa9519e422c5a8b7dd82c64979dc11db
jpnsantoss <jpnsantos14@gmail.com>
2024-02-27 16:18:52 +0000

    navbar

---
+++ app/(dashboard)/_components/invite-button.tsx
@@ -0,0 +1,37 @@
+import { OrganizationProfile } from "@clerk/nextjs";
+import { Plus } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+
+export const InviteButton = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">
+          <Plus className="h-4 w-4 mr-2" />
+          Invite members
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="p-0 bg-transparent border-none max-w-[880px]">
+        <OrganizationProfile
+          appearance={{
+            elements: {
+              rootBox: {
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+                overflow: "hidden",
+              },
+              card: {
+                width: "100%",
+                maxWidth: "880px",
+              },
+            },
+          }}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+};
