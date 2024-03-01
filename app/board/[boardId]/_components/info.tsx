e96db0a7c88294663383c7543dd990cde0a955e2
jpnsantoss <jpnsantos14@gmail.com>
2024-03-01 16:21:47 +0000

    canvas info

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,10 +1,61 @@
 "use client";
+import { Hint } from "@/components/hint";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 import { Skeleton } from "@/components/ui/skeleton";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { cn } from "@/lib/utils";
+import { useRenameModal } from "@/store/use-rename-modal";
+import { useQuery } from "convex/react";
+import { Poppins } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
 
-export const Info = () => {
+interface InfoProps {
+  boardId: string;
+}
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
+export const Info = ({ boardId }: InfoProps) => {
+  const data = useQuery(api.board.get, { id: boardId as Id<"boards"> });
+
+  const { onOpen } = useRenameModal();
+
+  if (!data) return <InfoSkeleton />;
   return (
     <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
-      TODO: Information about the board
+      <Hint label="Go to boards" side="bottom" sideOffset={10}>
+        <Button asChild variant="board" className="px-2">
+          <Link href="/">
+            <Image src="/logo.svg" alt="Board logo" height={40} width={40} />
+            <span
+              className={cn(
+                "font-semibold text-xl ml-2 text-black",
+                font.className
+              )}
+            >
+              Board
+            </span>
+          </Link>
+        </Button>
+      </Hint>
+      <Separator
+        orientation="vertical"
+        className="max-h-6 bg-neutral-300 mx-1.5"
+      />
+      <Hint label="Edit title" side="bottom" sideOffset={10}>
+        <Button
+          variant="board"
+          onClick={() => onOpen(data._id, data.title)}
+          className="text-base font-normal px-2"
+        >
+          {data.title}
+        </Button>
+      </Hint>
     </div>
   );
 };
