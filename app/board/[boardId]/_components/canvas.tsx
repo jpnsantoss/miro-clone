d609044b472002160d0c9bf27308b7c300c64102
jpnsantoss <jpnsantos14@gmail.com>
2024-03-05 16:05:54 +0000

    selection box

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -25,6 +25,7 @@ import { CursorsPresence } from "./cursors-presence";
 import { Info } from "./info";
 import { LayerPreview } from "./layer-preview";
 import { Participants } from "./participants";
+import { SelectionBox } from "./selection-box";
 import { Toolbar } from "./toolbar";
 
 const MAX_LAYERS = 100;
@@ -198,6 +199,7 @@ export const Canvas = ({ boardId }: CanvasProps) => {
               selectionColor={layerIdsToColorSelection[layerId]}
             />
           ))}
+          <SelectionBox onResizeHandlePointerDown={() => {}} />
           <CursorsPresence />
         </g>
       </svg>
