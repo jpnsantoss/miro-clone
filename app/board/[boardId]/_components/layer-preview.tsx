46ef2094cc395c126d332f78bc1f9c2749307f77
jpnsantoss <jpnsantos14@gmail.com>
2024-03-06 17:36:08 +0000

    ellipse

--- app/board/[boardId]/_components/layer-preview.tsx
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -5,6 +5,7 @@ import { memo } from "react";
 import { useStorage } from "@/liveblocks.config";
 import { LayerType } from "@/types/canvas";
 
+import { Ellipse } from "./ellipse";
 import { Rectangle } from "./rectangle";
 
 interface LayerPreviewProps {
@@ -22,6 +23,15 @@ export const LayerPreview = memo(
     }
 
     switch (layer.type) {
+      case LayerType.Ellipse:
+        return (
+          <Ellipse
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointerDown}
+            selectionColor={selectionColor}
+          />
+        );
       case LayerType.Rectangle:
         return (
           <Rectangle
